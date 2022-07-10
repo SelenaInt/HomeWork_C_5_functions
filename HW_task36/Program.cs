d290bf3818a31ea8af64d9b0211d1c28fe41526f
SelenaInt <selen15@yandex.ru>
2022-07-10 21:46:12 +0500

    HW_task36: sum of elements in odd positions

---
+++ HW_task36/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.WriteLine("Введите число определяющее размер массива");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+
+int[] array = new int[sizeArray];
+int count = 0;
+int sumOfArrayElements = 0;
+
+for (int index = 0; index < array.Length; index++)
+{
+    array[index] = new Random().Next(-1000,1000);
+}
+Console.WriteLine($"Исходный Массив: [{String.Join("; ", array)}]");
+
+for (int index = 0; index < array.Length; index++)
+{
+    if (index % 2 != 0)
+    {
+        sumOfArrayElements+= array[index];
+        count++;
+    }
+}
+Console.WriteLine($"Количество Нечетных позиций в данном диапазоне: {count}");
+Console.WriteLine($"Сумма элементов на четных позициях: {sumOfArrayElements}");
